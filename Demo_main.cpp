3a5b881260090ac9c6327154c7c2dece2d0614f4
SamaMohsen11 <samamohsen517@gmail.com>
2025-11-21 11:48:18 +0200

    add O

--- Demo_main.cpp
+++ Demo_main.cpp
@@ -7,8 +7,6 @@
 
 #include "BoardGame_Classes.h"
 #include "XO_Classes.h"
-#include"C:\Users\dell\source\repos\Asss3.opp\Asss3\Numerical_TicTacToe_UI.h"
-#include"C:\Users\dell\source\repos\Asss3.opp\Asss3\Numerical_TicTacToe_Board.h"
 #include"C:\Users\dell\source\repos\Asss3.opp\Asss3\Misere_Board.h"
 #include"C:\Users\dell\source\repos\Asss3.opp\Asss3\Misere_UI.h"
 #include"C:\Users\dell\source\repos\Asss3.opp\Asss3\Obstacles_Board.h"
@@ -24,7 +22,6 @@ int main() {
     int choice;
     cout << "Choose a game:\n";
     cout << "1. X-O Game\n";
-    cout << "2. Numerical Tic-Tac-Toe\n";
     cout << "3. Misere Tic-Tac-Toe";
     cout << "4. Obstacles Tic-Tac-Toe\n";
     cout << "Enter your choice: ";
@@ -38,20 +35,6 @@ int main() {
         Player<char>** players = game_ui->setup_players();
 
         GameManager<char> game(board, players, game_ui);
-        game.run();
-
-        delete board;
-        for (int i = 0; i < 2; ++i) delete players[i];
-        delete[] players;
-        delete game_ui;
-    }
-     if (choice == 2) {
-        // ---------- Numerical Tic-Tac-Toe ----------
-        UI<int>* game_ui = new Numerical_TicTacToe_UI();
-        Board<int>* board = new Numerical_TicTacToe_Board();
-        Player<int>** players = game_ui->setup_players();
-
-        GameManager<int> game(board, players, game_ui);
         game.run();
 
         delete board;
