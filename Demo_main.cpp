9f458cb192fc0a79ae51604955cd30e7835ca536
SamaMohsen11 <samamohsen517@gmail.com>
2025-11-21 16:43:07 +0200

    Update Demo_main.cpp

--- Demo_main.cpp
+++ Demo_main.cpp
@@ -7,10 +7,10 @@
 
 #include "BoardGame_Classes.h"
 #include "XO_Classes.h"
-#include"C:\Users\dell\source\repos\Asss3.opp\Asss3\Misere_Board.h"
-#include"C:\Users\dell\source\repos\Asss3.opp\Asss3\Misere_UI.h"
-#include"C:\Users\dell\source\repos\Asss3.opp\Asss3\Obstacles_Board.h"
-#include"C:\Users\dell\source\repos\Asss3.opp\Asss3\Obstacles_UI.h"
+#include"C:\Users\dell\source\repos\Asss3.opp\Asss3\Misere_TicTacToe_Board.h"
+#include"C:\Users\dell\source\repos\Asss3.opp\Asss3\Misere_TicTacToe_UI.h"
+#include"C:\Users\dell\source\repos\Asss3.opp\Asss3\Obstacles_TicTacToe_Board.h"
+#include"C:\Users\dell\source\repos\Asss3.opp\Asss3\Obstacles_TicTacToe_UI.h"
 
 using namespace std;
 
@@ -42,33 +42,33 @@ int main() {
         delete[] players;
         delete game_ui;
     }
-     // Misere Tic-tac-Toe//
-     if (choice == 3) {
-         UI<char>* game_ui = new Misere_UI();
-         Board<char>* board = new Misere_Board();
-         Player<char>** players = game_ui->setup_players();
+    //Misere Tic-tac-Toe//
+if (choice == 3) {
+    UI<char>* ui = new Misere_TicTacToe_UI();
+    Board<char>* board = new Misere_TicTacToe_Board();
+    Player<char>** players = ui->setup_players();
 
-         GameManager<char> game(board, players, game_ui);
-         game.run();
-
-         delete board;
-         for (int i = 0; i < 2; ++i) delete players[i];
-         delete[] players;
-         delete game_ui;
-     }
-         //Obstacles Tic-tac-Toe//
- if (choice == 4) {
-    UI<char>* game_ui = new Obstacles_UI();
-    Board<char>* board = new Obstacles_Board();
-    Player<char>** players = game_ui->setup_players();
-
-    GameManager<char> game(board, players, game_ui);
+    GameManager<char> game(board, players, ui);
     game.run();
 
     delete board;
-    for (int i = 0; i < 2; ++i) delete players[i];
+    for (int i = 0; i < 2; i++) delete players[i];
     delete[] players;
-    delete game_ui;
+    delete ui;
+}
+//Obstacles Tic-tac-Toe//
+if (choice == 4) {
+    UI<char>* ui = new Obstacles_TicTacToe_UI();
+    Board<char>* board = new Obstacles_TicTacToe_Board();
+    Player<char>** players = ui->setup_players();
+
+    GameManager<char> game(board, players, ui);
+    game.run();
+
+    delete board;
+    for (int i = 0; i < 2; i++) delete players[i];
+    delete[] players;
+    delete ui;
 }
      else {
          cout << "Invalid choice. Exiting...\n";
