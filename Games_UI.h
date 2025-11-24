ba9bde4c0d6dae87e577f9af50ad3d7028d9b05b
farahibrahim776 <farahibrahim776@gmail.com>
2025-11-24 14:41:52 +0200

    Update Games_UI.h
    AddAdd Numerical TicTacToe_UI.h to Games_UI.h

--- Games_UI.h
+++ Games_UI.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "BoardGame_Classes.h"
+#include "TicTacToe4x4_Board.h"
+#include "Numerical_TicTacToe_Board.h"
 #include <iostream>
 #include <string>
 #include <cstdlib>
@@ -27,11 +29,19 @@ public:
     Move<char>* get_move(Player<char>* player) override;
     void display_final_result(TicTacToe5x5_Board* board, Player<char>* p1, Player<char>* p2);
 };
+
+// 4x4TicTacToe
 class TicTacToe4x4_UI : public UI<char> {
 public:
     TicTacToe4x4_UI();
     Player<char>* create_player(string& name, char symbol, PlayerType type) override;
     Move<char>* get_move(Player<char>* player) override;
+};
 
-
+//Numerical TicTacToe
+class Numerical_TicTacToe_UI : public UI<int> {
+public:
+    Numerical_TicTacToe_UI();
+    Player<int>* create_player(string& name, int symbol, PlayerType type) override;
+    Move<int>* get_move(Player<int>* player) override;
 };
