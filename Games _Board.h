72b00d4702d7fef44a157c7e0fa84c683d0afb82
judyy55 <jodyatef88888@gmail.com>
2025-11-24 20:46:56 +0200

    Add SUS_Board class for SUS game implementation

--- Games _Board.h
+++ Games _Board.h
@@ -89,6 +89,23 @@ public:
 
 };
 
+// SUS game //
+class SUS_Board : public Board<char> {
+private:
+    char blank_symbol = '.';
+
+public:
+    SUS_Board();
+
+    bool update_board(Move<char>* move) override;
+    bool is_win(Player<char>* player) override;     
+    bool is_lose(Player<char>* player) override;
+    bool is_draw(Player<char>* player) override;
+    bool game_is_over(Player<char>* player) override;
+    void display_scores();
+    int count_SUS_sequences(char symbol);
+};
+
 
 
 
