caaadc8e0f9f6e37740e8f7f59d6ec820a3bd286
judyy55 <jodyatef88888@gmail.com>
2025-12-06 02:07:50 +0200

    Implement minimax algorithm for AI moves

--- SUS_Game.cpp
+++ SUS_Game.cpp
@@ -135,6 +135,89 @@ void SUS_Board::display_scores() {
 
 
 
+int SUS_Board::evaluate() const {
+    int s = 0, u = 0;
+
+    // count SUS sequences
+    // rows
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == 'S' && board[i][1] == 'U' && board[i][2] == 'S') s++;
+    }
+    // columns
+    for (int j = 0; j < 3; j++) {
+        if (board[0][j] == 'S' && board[1][j] == 'U' && board[2][j] == 'S') s++;
+    }
+    // diagonals
+    if (board[0][0] == 'S' && board[1][1] == 'U' && board[2][2] == 'S') s++;
+    if (board[0][2] == 'S' && board[1][1] == 'U' && board[2][0] == 'S') s++;
+
+    return s;
+}
+int SUS_Board::minimax(int depth, char aiSymbol, bool isMaximizing) {
+    // terminal condition
+    if (game_is_over(nullptr)) {
+        return evaluate();
+    }
+
+    char opp = (aiSymbol == 'S' ? 'U' : 'S');
+
+    if (isMaximizing) {
+        int best = -1000;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '.') {
+                    board[i][j] = aiSymbol;
+                    int value = minimax(depth + 1, aiSymbol, false);
+                    board[i][j] = '.';
+                    best = max(best, value);
+                }
+            }
+        }
+        return best;
+    }
+    else {
+        int best = 1000;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '.') {
+                    board[i][j] = opp;
+                    int value = minimax(depth + 1, aiSymbol, true);
+                    board[i][j] = '.';
+                    best = min(best, value);
+                }
+            }
+        }
+        return best;
+    }
+}
+void SUS_Board::get_best_move(char aiSymbol, int& bestX, int& bestY) {
+    int bestVal = -1000;
+
+    bestX = -1;
+    bestY = -1;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] == '.') {
+                board[i][j] = aiSymbol;
+
+                int moveVal = minimax(0, aiSymbol, false);
+
+                board[i][j] = '.';
+
+                if (moveVal > bestVal) {
+                    bestX = i;
+                    bestY = j;
+                    bestVal = moveVal;
+                }
+            }
+        }
+    }
+}
+
+
 // SUS_UI Implementation //
 
 SUS_UI::SUS_UI() : UI<char>("Welcome to SUS Game!", 3) {}
@@ -153,13 +236,12 @@ Move<char>* SUS_UI::get_move(Player<char>* player) {
         cin >> x >> y;
     }
     else {
-        Board<char>* board = player->get_board_ptr();
-        do {
-            x = rand() % 3;
-            y = rand() % 3;
-        } while (board->get_board_matrix()[x][y] != '.');
+        Board<char>* b = player->get_board_ptr();
+        SUS_Board* sb = dynamic_cast<SUS_Board*>(b);
 
-        cout << player->get_name() << " (Computer) played (" << x << ", " << y << ")\n";
+        sb->get_best_move(player->get_symbol(), x, y);
+
+        cout << player->get_name() << " (Computer AI) played (" << x << ", " << y << ")\n";
     }
 
     return new Move<char>(x, y, player->get_symbol());
