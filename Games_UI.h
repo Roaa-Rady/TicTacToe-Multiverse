a4a72ff27d633d650dbed49a722defba905caf15
SamaMohsen11 <samamohsen517@gmail.com>
2025-12-08 20:44:00 +0200

    documentation Games_UI.h

--- Games_UI.h
+++ Games_UI.h
@@ -5,129 +5,198 @@
 #include <string>
 #include <cstdlib>
 using namespace std;
-//Misere game//
+/// @brief Misere Tic-Tac-Toe UI class
 class Misere_TicTacToe_UI : public UI<char> {
 public:
+    /// @brief Constructor
     Misere_TicTacToe_UI();
+
+    /// @brief Creates a player for the game
     Player<char>* create_player(string& name, char symbol, PlayerType type) override;
+
+    /// @brief Gets a move from the player
     Move<char>* get_move(Player<char>* player) override;
 };
-//Obstacles game//
+
+/// @brief Obstacles Tic-Tac-Toe UI class
 class Obstacles_TicTacToe_UI : public UI<char> {
 public:
+    /// @brief Constructor
     Obstacles_TicTacToe_UI();
+
+    /// @brief Creates a player for the game
     Player<char>* create_player(string& name, char symbol, PlayerType type) override;
+
+    /// @brief Gets a move from the player
     Move<char>* get_move(Player<char>* player) override;
 };
-//5x5 Tic-tac-Toe
+
+/// @brief 5x5 Tic-Tac-Toe UI class
 class TicTacToe5x5_UI : public UI<char> {
 public:
-    TicTacToe5x5_UI();  // Constructor
+    /// @brief Constructor
+    TicTacToe5x5_UI();
 
+    /// @brief Creates a player for the game
     Player<char>* create_player(string& name, char symbol, PlayerType type) override;
+
+    /// @brief Gets a move from the player
     Move<char>* get_move(Player<char>* player) override;
+
+    /// @brief Displays the final result of the game
     void display_final_result(TicTacToe5x5_Board* board, Player<char>* p1, Player<char>* p2);
 };
 
-// 4x4TicTacToe
+/// @brief 4x4 Tic-Tac-Toe UI class
 class TicTacToe4x4_UI : public UI<char> {
 public:
+    /// @brief Constructor
     TicTacToe4x4_UI();
+
+    /// @brief Creates a player for the game
     Player<char>* create_player(string& name, char symbol, PlayerType type) override;
+
+    /// @brief Gets a move from the player
     Move<char>* get_move(Player<char>* player) override;
 };
 
-//Numerical TicTacToe
-class Numerical_TicTacToe_UI : public UI<char> {
+/// @brief Numerical Tic-Tac-Toe UI class
+class Numerical_TicTacToe_UI : public UI<int> {
 public:
+    /// @brief Constructor
     Numerical_TicTacToe_UI();
-    
+
+    /// @brief Creates a player for the game
+    Player<int>* create_player(string& name, int symbol, PlayerType type) override;
+
+    /// @brief Gets a move from the player
+    Move<int>* get_move(Player<int>* player) override;
+};
+
+/// @brief SUS Tic-Tac-Toe UI class
+class SUS_UI : public UI<char> {
+public:
+    /// @brief Constructor
+    SUS_UI();
+
+    /// @brief Creates a player for the game
     Player<char>* create_player(string& name, char symbol, PlayerType type) override;
+
+    /// @brief Gets a move from the player
+    Move<char>* get_move(Player<char>* player) override;
+
+    /// @brief Sets up the players
+    Player<char>** setup_players() override;
+};
+
+/// @brief Diamond Tic-Tac-Toe UI class
+class Diamond_UI : public UI<char> {
+public:
+    /// @brief Constructor
+    Diamond_UI();
+
+    /// @brief Creates a player for the game
+    Player<char>* create_player(string& name, char symbol, PlayerType type) override;
+
+    /// @brief Gets a move from the player
     Move<char>* get_move(Player<char>* player) override;
 };
 
-//Pyramid TicTacToe
-class Pyramid_TicTacToe_UI : public UI<char> {
+/// @brief Ultimate Tic-Tac-Toe UI class
+class Ultimate_UI : public UI<char> {
 public:
-    Pyramid_TicTacToe_UI();
+    /// @brief Constructor
+    Ultimate_UI();
+
+    /// @brief Creates a player for the game
     Player<char>* create_player(string& name, char symbol, PlayerType type) override;
+
+    /// @brief Gets a move from the player
     Move<char>* get_move(Player<char>* player) override;
+};
+
+/// @brief Infinity Tic-Tac-Toe UI class
+class InfinityTicTacToe_UI : public UI<char> {
+public:
+    /// @brief Constructor
+    InfinityTicTacToe_UI();
+
+    /// @brief Creates a player for the game
+    Player<char>* create_player(string& name, char symbol, PlayerType type) override;
+
+    /// @brief Gets a move from the player
+    Move<char>* get_move(Player<char>* player) override;
+
+    /// @brief Displays the final result of the game
+    void display_final_result(Player<char>* p1, Player<char>* p2);
+
+    /// @brief Sets up the players
+    Player<char>** setup_players() override;
+};
+
+/// @brief Memory Tic-Tac-Toe UI class
+class Memory_TicTacToe_UI : public UI<char> {
+public:
+    /// @brief Constructor
+    Memory_TicTacToe_UI();
+
+    /// @brief Gets a move from the player
+    Move<char>* get_move(Player<char>*) override;
+
+    /// @brief Creates a player for the game
+    Player<char>* create_player(string& name, char symbol, PlayerType type) override;
+
+    /// @brief Displays the board matrix
     void display_board_matrix(const vector<vector<char>>& matrix) const override;
 };
 
-
-// SUS Game //
-class SUS_UI : public UI<char> {
-public:
-    SUS_UI();
-
-    Player<char>* create_player(string& name, char symbol, PlayerType type) override;
-    Move<char>* get_move(Player<char>* player) override;
-    void display_final_result(Player<char>* p1, Player<char>* p2); 
-    Player<char>** setup_players() override;
-};
-
-
-// FourInARow Game //
-class FourInARowUI : public UI<char> {
-public:
-    FourInARowUI();
-
-    Player<char>* create_player(string& name, char symbol, PlayerType type) override;
-    Move<char>* get_move(Player<char>* player) override;
-    void display_final_result(Player<char>* p1, Player<char>* p2);
-
-    Player<char>** setup_players() override;
-};
-
-
-// Infinity Tic-Tac-Toe //
-class InfinityTicTacToe_UI : public UI<char> {
-public:
-    InfinityTicTacToe_UI();
-
-    Player<char>* create_player(string& name, char symbol, PlayerType type) override;
-    Move<char>* get_move(Player<char>* player) override;
-    void display_final_result(Player<char>* p1, Player<char>* p2);
-    Player<char>** setup_players() override;
-};
-
-//Word Tic-Tac-Toe
+/// @brief Word Tic-Tac-Toe UI class
 class Word_TicTacToe_UI : public UI<char> {
 public:
+    /// @brief Constructor
     Word_TicTacToe_UI();
+
+    /// @brief Creates a player for the game
     Player<char>* create_player(std::string& name, char symbol, PlayerType type) override;
+
+    /// @brief Gets a move from the player
     Move<char>* get_move(Player<char>* player) override;
+
+    /// @brief Shows the result of the game
     void show_result(const Word_TicTacToe_Board* board, Player<char>* winner) const;
 };
 
-//Diamond game//
-class Diamond_UI : public UI<char> {
+/// @brief Four-in-a-Row UI class
+class FourInARowUI : public UI<char> {
 public:
-    Diamond_UI();
+    /// @brief Constructor
+    FourInARowUI();
 
+    /// @brief Creates a player for the game
     Player<char>* create_player(string& name, char symbol, PlayerType type) override;
 
+    /// @brief Gets a move from the player
     Move<char>* get_move(Player<char>* player) override;
+
+    /// @brief Displays the final result of the game
+    void display_final_result(Player<char>* p1, Player<char>* p2);
+
+    /// @brief Sets up the players
+    Player<char>** setup_players() override;
 };
 
-//Memory TicTacToe
-class Memory_TicTacToe_UI : public UI<char> {
+/// @brief Pyramid Tic-Tac-Toe UI class
+class Pyramid_TicTacToe_UI : public UI<char> {
 public:
-	Memory_TicTacToe_UI();
-    Move<char>* get_move(Player<char>*) override;
+    /// @brief Constructor
+    Pyramid_TicTacToe_UI();
+
+    /// @brief Creates a player for the game
     Player<char>* create_player(string& name, char symbol, PlayerType type) override;
 
+    /// @brief Gets a move from the player
+    Move<char>* get_move(Player<char>* player) override;
+
+    /// @brief Displays the board matrix
     void display_board_matrix(const vector<vector<char>>& matrix) const override;
-
-};
-
-class Ultimate_UI : public UI<char> {
-public:
-    Ultimate_UI();
-
-    Player<char>* create_player(string& name, char symbol, PlayerType type) override;
-    Move<char>* get_move(Player<char>* player) override;
-
-    // void display_main_and_subboards(Ultimate_Board* board);
 };
